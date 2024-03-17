6d3167514ab8ed710fdb9b2492779b340597d741
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-03-17 02:44:09 +0200

    add

--- script/script_index.js
+++ script/script_index.js
@@ -225,5 +225,5 @@ document.addEventListener("DOMContentLoaded", function() {
   
 
   function go_to() {
-    window.location.href = 'https://amira-ahmed2.github.io/sellastore/detiles.html'; // استبدل الرابط الجديد هنا
+    window.location.href = 'https://amira-ahmed2.github.io/sellastore/detiles.html';
 }
